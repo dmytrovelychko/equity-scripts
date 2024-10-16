f27f110d6bea35241bc21ece704004ac8c9ce43e
dmytrovelychko <dmytro.velychko.pro@gmail.com>
2024-10-16 18:56:12 +0300

    fix db clean up

--- scripts/clean_db.py
+++ scripts/clean_db.py
@@ -1,4 +1,11 @@
 import psycopg2
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+import time
+
+logging.info("Waiting for db container up...")
+time.sleep(5)
 
 conn = psycopg2.connect(database = "equity_calculator", 
                         user = "postgres", 
@@ -10,7 +17,7 @@ cur = conn.cursor()
 query = "TRUNCATE TABLE Equity"
 
 try:
-    cur.execute(query, (First, Second))
+    cur.execute(query)
 except Exception as e:
     conn.rollback()
     print(f"An error occurred: {e}")
